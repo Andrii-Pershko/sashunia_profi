5db8347e6d733a7af829fd045925f678c652a9e1
Andrii-Pershko <andriiinf@gmail.com>
2024-06-20 22:17:48 +0300

    правив баг

--- js/teamScroll.js
+++ js/teamScroll.js
@@ -1,27 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
   const container = document.querySelector('.team-list');
-  const parent = container.parentElement;
   let isScrolling;
+  let targetScrollLeft;
 
-  const handleWheel = event => {
-    const atStart = container.scrollLeft === 0;
-    const atEnd =
-      container.scrollLeft + container.clientWidth >= container.scrollWidth;
-
-    // Якщо горизонтальний скрол досягнув краю, дозволити вертикальний скрол
-    if ((event.deltaY < 0 && atStart) || (event.deltaY > 0 && atEnd)) {
-      parent.scrollTop += event.deltaY;
-      return;
+  container.addEventListener('wheel', function (event) {
+    // Перевірка чи потрібно скролити вертикально
+    if (
+      (event.deltaY < 0 && container.scrollLeft === 0) ||
+      (event.deltaY > 0 &&
+        container.scrollLeft + container.clientWidth >= container.scrollWidth)
+    ) {
+      return; // Дозволити вертикальний скрол
     }
 
-    // Призначення горизонтального скролу
     event.preventDefault();
 
     if (isScrolling) {
       window.cancelAnimationFrame(isScrolling);
     }
 
-    let targetScrollLeft = container.scrollLeft + event.deltaY;
+    targetScrollLeft = container.scrollLeft + event.deltaY;
 
     const scroll = () => {
       const currentScrollLeft = container.scrollLeft;
@@ -38,44 +36,5 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     scroll();
-  };
-
-  const handleParentWheel = event => {
-    // Дозволити вертикальний скрол тільки якщо горизонтальний скрол досягнув краю
-    if (
-      (event.deltaY < 0 && container.scrollLeft === 0) ||
-      (event.deltaY > 0 &&
-        container.scrollLeft + container.clientWidth >= container.scrollWidth)
-    ) {
-      parent.scrollTop += event.deltaY;
-    } else {
-      // Призначення горизонтального скролу
-      event.preventDefault();
-
-      if (isScrolling) {
-        window.cancelAnimationFrame(isScrolling);
-      }
-
-      let targetScrollLeft = container.scrollLeft + event.deltaY;
-
-      const scroll = () => {
-        const currentScrollLeft = container.scrollLeft;
-        const distance = targetScrollLeft - currentScrollLeft;
-        const step = distance / 10;
-
-        if (Math.abs(step) > 1) {
-          container.scrollLeft += step;
-          isScrolling = window.requestAnimationFrame(scroll);
-        } else {
-          container.scrollLeft = targetScrollLeft;
-          isScrolling = null;
-        }
-      };
-
-      scroll();
-    }
-  };
-
-  container.addEventListener('wheel', handleWheel, { passive: false });
-  parent.addEventListener('wheel', handleParentWheel, { passive: false });
+  });
 });
